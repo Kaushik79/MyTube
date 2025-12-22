be5a1d71286d163ed934a4304b315d7631365b9a
Kaushik79 <supersk240k@gmail.com>
2025-12-22 13:36:08 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Thumbnail from './components/thumbnail';
+
+const App = () => {
+  return (
+    <div className=''>
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Thumbnail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Thumbnail = () => {
+    return(
+        <div className='bg-black w-40 h-28 sm:w-68 sm:h-44 lg:w-90 lg:h-60 rounded-lg'>
+            
+        </div>
+    );
+};
+
+export default Thumbnail;
